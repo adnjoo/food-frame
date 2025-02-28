fe814174d9df3d4940e18520ce1e8394cead71a8
Andrew Njoo <adnjoo@gmail.com>
2025-02-28 12:44:15 +0700

    container

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { Container } from 'components/Container';
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
@@ -5,9 +6,9 @@ import './global.css';
 
 export default function App() {
   return (
-    <>
+    <Container>
       <ScreenContent title="Home" path="App.tsx" />
       <StatusBar style="auto" />
-    </>
+    </Container>
   );
 }
