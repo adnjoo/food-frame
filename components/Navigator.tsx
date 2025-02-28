98d5a84065054b274e9296e6e8c6db0651de1b8d
Andrew Njoo <adnjoo@gmail.com>
2025-02-28 17:55:30 +0700

    settings (#23)
    * qwe
    
    * a

--- components/Navigator.tsx
+++ components/Navigator.tsx
@@ -5,6 +5,7 @@ import { SafeAreaView, View } from 'react-native';
 import BottomNav from './BottomNav';
 import AnalyticsScreen from './screens/Analytics';
 import HomeScreen from './screens/Home';
+import SettingsScreen from './screens/Settings';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,9 +14,10 @@ export default function Navigator() {
     <NavigationContainer>
       <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
         <View style={{ flex: 1 }}>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Analytics" component={AnalyticsScreen} />
+            <Stack.Screen name="Settings" component={SettingsScreen} />
           </Stack.Navigator>
           <BottomNav />
         </View>
